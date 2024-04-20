1c0e48301ada7c1f92a85b6b6d5cd40601e04df3
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-04-20 18:59:04 -0300

    feat: tratando erros

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,4 +1,5 @@
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository';
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists';
 import { RegisterUseCase } from '@/use-cases/register';
 import { FastifyReply, FastifyRequest } from 'fastify';
 import { z } from 'zod';
@@ -16,7 +17,10 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 		const registerUseCase = new RegisterUseCase(usersRepository);
 		await registerUseCase.execute({ name, email, password });
 	} catch (error) {
-		return reply.status(409).send();
+		if (error instanceof UserAlreadyExistsError) {
+			return reply.status(409).send({ message: error.message });
+		}
+		return reply.status(500).send();
 	}
 
 	return reply.status(201).send();

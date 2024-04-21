14ae64abaa09aff2670e282b22d8039820fc1ba3
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-04-21 11:04:28 -0300

    feat: tratando erros

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,21 @@
 import fastify from 'fastify';
 import { AppRoutes } from './http/routes';
+import { ZodError } from 'zod';
+import { env } from './env';
 
 export const app = fastify();
 app.register(AppRoutes);
+
+app.setErrorHandler((error, _, reply) => {
+	if (error instanceof ZodError) {
+		return reply
+        .status(400)
+        .send({message:'Validate error.', issues: error.format()})
+	}
+    if(env.NODE_ENV!== 'production'){
+        console.error(error)
+    }else{
+        //TODO: here we shold log to an external tool like Datadog
+    }
+    return reply.status(500).send({message:'Internal server error.'})
+});

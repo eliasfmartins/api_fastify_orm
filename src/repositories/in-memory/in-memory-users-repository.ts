5f0fce7ddb5729d9e6fa5279ebb1ccc73ce2f895
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-05-05 14:59:22 -0300

    feat: create method findById

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -4,6 +4,14 @@ import { UserRepository } from '../users-repository';
 export class InMemoryUserRepository implements UserRepository {
 	public items: User[] = [];
 
+	async findById(id: string) {
+		const user = this.items.find((item) => item.id === id);
+		if (!user) {
+			return null;
+		}
+		return user;
+	}
+
 	async findByEmail(email: string) {
 		const user = this.items.find((item) => item.email === email);
 		if (!user) {

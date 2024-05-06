eec744660b19cd202cd1ecf511234096aa46b8e2
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-05-06 20:33:36 -0300

    feat: implementano teste date

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -1,14 +1,23 @@
 import { CheckIn, Prisma, User } from '@prisma/client';
 import { CheckInsRepository } from '../check-ins-repository';
 import { randomUUID } from 'node:crypto';
+import dayjs from 'dayjs';
 
 export class InMemoryCheckInsRepository implements CheckInsRepository {
 	public items: CheckIn[] = [];
 
 	async findByUserIdOnDate(userId: string, date: Date) {
-		const checkInOnSameDate = this.items.find(
-			(check) => check.user_id === userId
-		);
+		const startOfTheDay = dayjs(date).startOf('date');
+		const endOfTheDay = dayjs(date).endOf('date');
+
+		const checkInOnSameDate = this.items.find((checkIn) => {
+			const checkInDate = dayjs(checkIn.created_at);
+
+			const IsOnSameDate =
+				checkInDate.isAfter(startOfTheDay) && checkInDate.isBefore(endOfTheDay);
+
+			return checkIn.user_id === userId && IsOnSameDate;
+		});
 		if (!checkInOnSameDate) {
 			return null;
 		}
--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -1,17 +1,32 @@
 import { expect, describe, it, beforeEach, vi, afterEach } from 'vitest';
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository';
 import { CheckInUseCase } from './chek-in';
-import { checkIn } from '@prisma/client';
+import { inMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository';
+import { Decimal } from '@prisma/client/runtime/library';
 
 let checkInsRepository: InMemoryCheckInsRepository;
+let gymsRepository: inMemoryGymsRepository;
 let sut: CheckInUseCase;
 
 describe('Check-in Use Case', () => {
 	beforeEach(() => {
+
 		checkInsRepository = new InMemoryCheckInsRepository();
-		sut = new CheckInUseCase(checkInsRepository);
+		gymsRepository =new inMemoryGymsRepository();
+		sut = new CheckInUseCase(checkInsRepository,gymsRepository );
+
+		gymsRepository.items.push({
+			id: 'gym-01',
+			title: 'JavaScript Gym',
+			description: '',
+			phone: '',
+			latitude: new Decimal(0),
+			longetude: new Decimal(0),
+		});
+
 		vi.useFakeTimers();
 	});
+
 	afterEach(() => {
 		vi.useRealTimers();
 	});
@@ -20,6 +35,8 @@ describe('Check-in Use Case', () => {
 		const { checkIn } = await sut.execute({
 			gymId: 'gym-01',
 			userId: 'user-01',
+			userLatitude: 0,
+			userLongetude: 0,
 		});
 		console.log(checkIn.created_at);
 
@@ -31,11 +48,15 @@ describe('Check-in Use Case', () => {
 		await sut.execute({
 			gymId: 'gym-01',
 			userId: 'user-01',
+			userLatitude: 0,
+			userLongetude: 0,
 		});
 		await expect(() =>
 			sut.execute({
 				gymId: 'gym-01',
 				userId: 'user-01',
+				userLatitude: 0,
+				userLongetude: 0,
 			})
 		).rejects.toBeInstanceOf(Error);
 	});
@@ -46,12 +67,16 @@ describe('Check-in Use Case', () => {
 		await sut.execute({
 			gymId: 'gym-01',
 			userId: 'user-01',
+			userLatitude: 0,
+			userLongetude: 0,
 		});
 
 		vi.setSystemTime(new Date(2022, 0, 23, 8, 0, 0));
 		const { checkIn } = await sut.execute({
 			gymId: 'gym-01',
 			userId: 'user-01',
+			userLatitude: 0,
+			userLongetude: 0,
 		});
 		expect(checkIn.id).toEqual(expect.any(String));
 	});

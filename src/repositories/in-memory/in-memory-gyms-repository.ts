6e7c2e9814d22ba645a4d3a3067cae606928a062
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-05-15 20:41:22 -0300

    feat: att im Memory Repository

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -14,7 +14,7 @@ export class inMemoryGymsRepository implements GymsRepository {
 	}
 	async create(data: Prisma.GymCreateInput) {
 		const gym = {
-			id: randomUUID(),
+			id: data.id ??  randomUUID(),
 			title: data.title,
 			description: data.description ?? null,
 			phone: data.phone ?? null,

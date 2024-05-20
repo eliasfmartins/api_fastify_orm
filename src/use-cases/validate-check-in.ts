bb9d7618db6d08aa7c9f96e94e13174bd26cf99f
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-05-20 19:49:50 -0300

    feat: valiadte checkin time

--- src/use-cases/validate-check-in.ts
+++ src/use-cases/validate-check-in.ts
@@ -1,6 +1,8 @@
 import { CheckIn } from '@prisma/client';
 import { CheckInsRepository } from '@/repositories/check-ins-repository';
 import { ResourceNotFOundError } from './errors/resource-nof-found-error';
+import dayjs from 'dayjs';
+import { LateCheckInValidationError } from './errors/late-check-in-validation-error';
 
 interface ValidateCheckInUseCaseRequest {
 	checkInId: string;
@@ -19,6 +21,14 @@ export class ValidateCheckInUseCase {
 			throw new ResourceNotFOundError();
 		}
 		checkIn.validated_at = new Date();
+		const distanceInMinutesFromCheckInCreation = dayjs(new Date()).diff(
+			checkIn.created_at,
+			'minutes'
+		);
+
+		if (distanceInMinutesFromCheckInCreation > 20){
+			throw new LateCheckInValidationError()
+		}
 
 		await this.CheckInsRepository.save(checkIn);
 		return { checkIn };

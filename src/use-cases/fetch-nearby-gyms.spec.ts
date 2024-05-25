36fe35d676a5452fd471c80ab40d656cfd7c1ac1
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-05-25 13:56:49 -0300

    configurando ambiente de testes interno

--- src/use-cases/fetch-nearby-gyms.spec.ts
+++ src/use-cases/fetch-nearby-gyms.spec.ts
@@ -1,15 +1,15 @@
 import { expect, describe, it, beforeEach } from 'vitest';
 
 import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository';
-import { FetchNearbyUseCase } from './fetch-nearby-gyms';
+import { FetchNearbyGymsUseCase } from './fetch-nearby-gyms';
 
 let gymsRepository: InMemoryGymsRepository;
-let sut: FetchNearbyUseCase;
+let sut: FetchNearbyGymsUseCase;
 
 describe('Fetch Nearby  Gyms Use Case', () => {
 	beforeEach(async () => {
 		gymsRepository = new InMemoryGymsRepository();
-		sut = new FetchNearbyUseCase(gymsRepository);
+		sut = new FetchNearbyGymsUseCase(gymsRepository);
 	});
 
 	it('shold be able to fech nearby gyms', async () => {

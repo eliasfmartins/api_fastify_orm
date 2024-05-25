e4fc290834212aa62bbd28de9a25ed9fcec0f0b0
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-05-25 12:38:56 -0300

    feat: implememntando JWT

--- src/app.ts
+++ src/app.ts
@@ -2,20 +2,24 @@ import fastify from 'fastify';
 import { AppRoutes } from './http/routes';
 import { ZodError } from 'zod';
 import { env } from './env';
+import fastifyJwt from '@fastify/jwt';
 
 export const app = fastify();
+app.register(fastifyJwt, {
+	secret: env.JWT_SECRET,
+});
 app.register(AppRoutes);
 
 app.setErrorHandler((error, _, reply) => {
 	if (error instanceof ZodError) {
 		return reply
-        .status(400)
-        .send({message:'Validate error.', issues: error.format()})
+			.status(400)
+			.send({ message: 'Validate error.', issues: error.format() });
 	}
-    if(env.NODE_ENV!== 'production'){
-        console.error(error)
-    }else{
-        //TODO: here we shold log to an external tool like Datadog
-    }
-    return reply.status(500).send({message:'Internal server error.'})
+	if (env.NODE_ENV !== 'production') {
+		console.error(error);
+	} else {
+		//TODO: here we shold log to an external tool like Datadog
+	}
+	return reply.status(500).send({ message: 'Internal server error.' });
 });

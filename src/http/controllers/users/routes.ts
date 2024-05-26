4543540b7bd358e460350872f2be8f70a4159273
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-05-26 12:16:04 -0300

    feat: refresh token e teste

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,10 +3,14 @@ import { profile } from './profile';
 import { register } from './register';
 import { FastifyInstance } from 'fastify';
 import { verifyJWT } from '../../middlewares/verify-jwt';
+import { aP } from 'vitest/dist/reporters-LqC_WI4d';
+import { refresh } from './refresh';
 
 export async function userRoutes(app: FastifyInstance) {
 	app.post('/users', register);
 	app.post('/sessions', authenticate);
+
+	app.patch('/token/refresh', refresh);
 	//  Authenticated
 	app.get('/me', { onRequest: [verifyJWT] }, profile);
 }
